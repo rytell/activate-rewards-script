7a8f50b7ab78abde1082061d189b226c75fc16ef
Pedro Medina <pedromedina190@gmail.com>
2021-11-30 19:52:02 -0300

    change provider to SES

--- sender.js
+++ sender.js
@@ -1,31 +1,39 @@
+const AWS = require('aws-sdk');
 
-// using Twilio SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
+const SES_CONFIG = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'us-east-1',
+};
 
-const sendError = async (error) => {
-  const sgMail = require('@sendgrid/mail')
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-  console.log(process.env.SENDGRID_API_KEY, ': SENDGRID API KEY')
+const AWS_SES = new AWS.SES(SES_CONFIG);
 
-  const recipients = ['xpinatapartydev@gmail.com', 'pluismedinaf@yahoo.com', 'pedromedina190@gmail.com'];
-
-  const msg = {
-    from: 'pedromedina190@gmail.com',
-    subject: 'Error at PartySwap Rewards Distribution',
-    text: error.toString()
-  }
-
-  try {
-    await Promise.all(recipients.map(recipient => {
-      sgMail.send({ ...msg, to: recipient });
-    }));
-  } catch (error) {
-    console.log(error, ': ERROR SENDING EMAIL');
-  }
-
-}
-
+const sendError = (error) => {
+  let params = {
+    Source: 'pedromedina190@gmail.com',
+    Destination: {
+      ToAddresses: [
+        'xpinatapartydev@gmail.com',
+        'Spicyforkbtc@gmail.com'
+      ],
+    },
+    ReplyToAddresses: [],
+    Message: {
+      Body: {
+        Html: {
+          Charset: 'UTF-8',
+          Data: error.toString(),
+        },
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: `Error Distributing Rewards for Party Piñatas`,
+      }
+    },
+  };
+  return AWS_SES.sendEmail(params).promise();
+};
 
 module.exports = {
-  sendError
-}
+  sendError,
+};
